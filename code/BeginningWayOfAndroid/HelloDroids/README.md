32bce2ee4280edae78c0612082235206f55474a7
Fred Grott <fred.grott@gmail.com>
2016-09-18 11:36:10 -0500

    added screen shot of app running to readme

---
+++ code/BeginningWayOfAndroid/HelloDroids/README.md
@@ -0,0 +1,31 @@
+# Hello Droids
+
+Simple Hello Droids app that displays a TextView with a string of text.
+Part of the BeginningWayOfAndroid book.
+
+![small screenshot](art/small-image-device-2016-09-18-112925.png)
+
+
+
+
+
+
+# Developed By
+
+![gws logo](art/gws_github_header.png)
+
+<a href="http://stackoverflow.com/users/237740/fred-grott">
+<img src="http://stackoverflow.com/users/flair/237740.png" width="208" height="58" alt="profile for Fred Grott at Stack Overflow, Q&amp;A for professional and enthusiast programmers" title="profile for Fred Grott at Stack Overflow, Q&amp;A for professional and enthusiast programmers">
+</a>
+
+
+Created by [Fred Grott](http://shareme.github.com).
+
+
+# Credits
+
+Original concept and code created by Google as part of API demos.
+
+# License
+
+Apache 2.0 License
---
+++ code/BeginningWayOfAndroid/HelloDroids/app/src/main/java/com/github/shareme/hellodroids/HelloDroids.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ * Modifications Copyright(C) 2016 Fred Grott(GrottWorkShop)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// all android apps have a package-name which usuallly is in the scheme of
+//    com,net, eu  than the firm or company than author or user and than the app name
+package com.github.shareme.hellodroids;
+
+// this is our imports for this class
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * There are two major View Component Containers in the Android
+ * Framework, Activities and Fragments.This is a basic AActivity
+ * displaying a simple text string via TextView which was declared
+ * in the xml file found in res/layouts/hello_droids.xml
+ *
+ * Note, to get the newer features of toolbar, ext you will use the
+ * AppCompatActivity from the support-v4 package which does subclass
+ * the Activity class.
+ * Created by fgrott on 9/18/2016.
+ */
+
+public class HelloDroids extends Activity {
+
+
+  /**
+   * This is what initializes the Activity and it must at least
+   * call the super and setContentView to display something.
+   *
+   * The order of the super method before other method calls is
+   * required. You will this pattern in Virtual Managed systems
+   * such as java platforms and the Android Platform. If for some reason
+   * the system is not ready to do the initialization of this activity, super
+   * class method is invoked so that some initialization occurs.
+   * @param savedInstanceState the saved instance state
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.hello_droids);
+  }
+}
